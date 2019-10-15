812287d74165e102ab6e8efec66e2d83536388ce
lenardpiir@gmail.com <lenardpiir@gmail.com>
2019-10-15 19:52:30 +0300

    Added map to transport feed

--- src/app/data-feeds/transport-feed/transport.component.ts
+++ src/app/data-feeds/transport-feed/transport.component.ts
@@ -1,24 +1,28 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {TransportService} from '../../shared/transport/transport.service';
-import {Subscription, timer} from 'rxjs';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-transport',
   templateUrl: './transport.component.html',
   styleUrls: ['./transport.component.css']
 })
-export class TransportComponent implements OnInit{
-  transport: Transport;
-  transportSub: Subscription;
-
-  constructor(private transportService: TransportService) {}
-
+export class TransportComponent implements OnInit {
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 37.75;
+  lng = -122.41;
+  constructor() { }
   ngOnInit() {
-      this.transportSub = this.transportService.transportSubject.subscribe(response => {
-          if (response) {
-            this.transport = response.transport;
-          }
-        });
+    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set('pk.eyJ1IjoibGVuYXJkMTI0IiwiYSI6ImNrMWtzcW93ZTFhZGEza2p5anlwdmlqdGoifQ.W-M2nFgwfGUs66suKydZxQ');
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: this.style,
+        zoom: 13,
+        center: [this.lng, this.lat]
+    });
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 }
+
 
