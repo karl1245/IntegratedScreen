1525614d5d1e985e965f7427a32ac608e457e94f
Silver <silvermaala@hotmail.com>
2019-11-11 08:26:36 +0200

    Added missing files from last commit.

---
+++ src/app/shared/saved-message/saved-message.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {savingAnimation} from '../../shared/animations';
+
+@Component({
+  selector: 'app-saved-message',
+  templateUrl: './saved-message.component.html',
+  styleUrls: ['./saved-message.component.css'],
+  animations: [
+    savingAnimation
+  ]
+})
+export class SavedMessageComponent implements OnInit {
+  @Input() message: string;
+  @Input() saving: boolean;
+  @Output() saved = new EventEmitter();
+
+  animationDone() {
+    this.saving = false;
+    this.saved.emit();
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
