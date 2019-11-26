934cebf53a21acd60a5e7ccbc11f1ef3ec238c1e
Silver <silvermaala@hotmail.com>
2019-11-26 13:24:05 +0200

    Added instructions for admin screens. Closes #14

--- src/app/shared/info-modal.service.ts
+++ src/app/shared/info-modal.service.ts
@@ -9,33 +9,62 @@ export class InfoModalService {
   dialogs = {
     video: {
       header: 'Video',
+      headerInfo: 'Here you can change what video is playing in the Information Screen.',
       info: [
-        'Here you can change what video is playing in the Information Screen.',
-        'You can add a Vimeo or a Youtube video link.',
-        'Copy the video link into the Video Location box.'
+        {
+          header:
+            'Instructions',
+          info: [
+            'Copy the Youtube or Vimeo link you want displayed into Video Location box and click save.'
+          ]
+        }
       ]
     },
     news: {
       header: 'News',
+      headerInfo: 'Here you can change what news sources are used to get the news.',
       info: [
-        'Here you can change what news sources are used to find the news.',
-        'This uses the News API (https://newsapi.org/).',
-        'The API key is needed to use the News feed.',
-        'You can get the key from here "https://newsapi.org/account".',
-        'You can select the news site where the get the top news.',
-        'Click on the news source to see more information about it or to remove it.'
+        {
+          header:
+            'API',
+          info: [
+            'This uses the News API (https://newsapi.org/).',
+            'The API key is needed to use the News feed.',
+            'You can get the key from here "https://newsapi.org/account".'
+          ]
+        },
+        {
+          header:
+            'Instructions',
+          info: [
+            'You can select the news site where the get the top news.',
+            'Click on the news source to see more information about it or to remove it.'
+          ]
+        }
       ]
     },
     weather: {
       header: 'Weather',
+      headerInfo: 'Here you can change the weather location and the units used.',
       info: [
-        'Here you can change the weather location and whether it is Celsius or Fahrenheit.',
-        'This uses the OpenWeather API (https://openweathermap.org/).',
-        'The API key is needed to use the News feed.',
-        'You can get the key from here "https://home.openweathermap.org/api_keys".',
-        'Type in the city and the units you want to be displayed and click save.'
+        {
+          header:
+            'API',
+          info: [
+            'This uses the OpenWeather API (https://openweathermap.org/).',
+            'The API key is needed to use the News feed.',
+            'You can get the key from here "https://home.openweathermap.org/api_keys".'
+          ]
+        },
+        {
+          header:
+            'Instructions',
+          info: [
+            'Type in the city and the units you want to be displayed and click save.'
+          ]
+        }
       ]
-    }
+    },
   };
 
   /**
@@ -43,7 +72,7 @@ export class InfoModalService {
    * @param dialog - MatDialog of the feed component.
    * @param data - the data that is displayed in the feed.
    */
-  private openInfo(dialog: MatDialog, data: {header: string, info: string[]}) {
+  private openInfo(dialog: MatDialog, data: {header: string, headerInfo: string, info: {header: string, info: string[]}[]}) {
     dialog.open(InfoModalComponent, {
       maxWidth: '80%',
       maxHeight: '80%',
@@ -51,14 +80,26 @@ export class InfoModalService {
     });
   }
 
+  /**
+   * Opens info model with video admin screen info.
+   * @param dialog - MatDialog of the feed component.
+   */
   openInfoVideo(dialog: MatDialog) {
     this.openInfo(dialog, this.dialogs.video);
   }
 
+  /**
+   * Opens info model with weather admin screen info.
+   * @param dialog - MatDialog of the feed component.
+   */
   openInfoWeather(dialog: MatDialog) {
     this.openInfo(dialog, this.dialogs.weather);
   }
 
+  /**
+   * Opens info model with news admin screen info.
+   * @param dialog - MatDialog of the feed component.
+   */
   openInfoNews(dialog: MatDialog) {
     this.openInfo(dialog, this.dialogs.news);
   }
