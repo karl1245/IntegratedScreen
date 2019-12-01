f04ebac4b9ba2a2863c968501b26cae3b68eb630
Silver <silvermaala@hotmail.com>
2019-12-01 15:19:19 +0200

    Added error handling for airport feed. Improved and commented code in airport service. Close #18

--- src/app/admin-screen/airport-admin/airport-admin.component.ts
+++ src/app/admin-screen/airport-admin/airport-admin.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {InfoModalService} from '../../shared/info-modal.service';
 import {FormControl, FormGroup} from '@angular/forms';
 import {AirportService} from '../../shared/airport/airport.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-airport-admin',
   templateUrl: './airport-admin.component.html',
   styleUrls: ['./airport-admin.component.css', '../admin-screen.component.css']
 })
-export class AirportAdminComponent implements OnInit {
+export class AirportAdminComponent implements OnInit, OnDestroy {
   airportForm: FormGroup;
 
   isArrival = true;
+
+  errorSub: Subscription;
+  errorMessage: string;
 
   airportSaved = false;
 
@@ -25,16 +29,26 @@ export class AirportAdminComponent implements OnInit {
     this.airportForm = new FormGroup({
       'isArrival': new FormControl(this.isArrival)
     });
+    this.errorSub = this.airportService.errorSubject.subscribe(error => {
+      this.errorMessage = error;
+    })
   }
 
   onSaveAorD() {
     this.airportSaved = true;
     this.isArrival = this.airportForm.value.isArrival;
     this.airportService.isArrival = this.airportForm.value.isArrival;
+    this.airportService.getAirport();
   }
 
   openInfo() {
     this.infoModalService.openInfoAirport(this.dialog);
   }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
+  }
+
+
 
 }
--- src/app/shared/airport/airport.service.ts
+++ src/app/shared/airport/airport.service.ts
@@ -4,7 +4,7 @@ import {HttpClient} from '@angular/common/http';
 import {map, take} from 'rxjs/operators';
 import {Arrival} from './arrival';
 import {Departure} from './departure';
-import {Subject} from 'rxjs';
+import {BehaviorSubject, Subject} from 'rxjs';
 
 /**
  * Service that handles Tallinn Airport related tasks.
@@ -17,6 +17,8 @@ export class AirportService {
 
   airportArrivalSubject = new Subject<Arrival[]>();
   airportDepartureSubject = new Subject<Departure[]>();
+
+  errorSubject = new BehaviorSubject<string>(null);
 
   constructor(private storageService: StorageService,
               private http: HttpClient) {
@@ -33,6 +35,9 @@ export class AirportService {
     this.storageService.saveAirport(value);
   }
 
+  /**
+   * Gets all arrivals or departures depending on user's choice.
+   */
   getAirport() {
     if (this.isArrival) {
       this.getArrivalsOutOfHTML();
@@ -41,92 +46,121 @@ export class AirportService {
     }
   }
 
+  /**
+   * Makes a http request to proxy server on localhost port 3000 and gets arrivals out of Tallinn airport website.
+   * Then sends it through airportArrivalSubject or airportDepartureSubject.
+   */
   private getArrivalsOutOfHTML() {
     this.http.get('http://localhost:3000/arrivals', {responseType: 'text'}).pipe(
       take(1),
       map(response => {
-        let arrivals = [];
-
-        const htmlString = response;
-        var htmlObject = document.createElement('div');
-        htmlObject.innerHTML = htmlString;
-        const table = this.getElementByAttribute("data-flights-type", "arrivals", htmlObject);
-        const rows = table.querySelectorAll('.t-row:not(.hidden)');
-
-        for(var i = 1; i < rows.length; i++) {
-          const row = rows[i];
-          if(row.getElementsByClassName("col-1-text").item(0) != null) {
-            const time = this.getTime(row);
-            const from = this.getToFrom(row);
-            const info = this.getInfo(row);
-            const flights = this.getFlights(row);
-            const airlines = this.getAirlines(row);
-            const arrival = new Arrival(time, from, flights, airlines, info);
-            arrivals.push(arrival)
-          }
-        }
-        return arrivals;
+        return this.getAorD(response);
       })
     ).subscribe(
       arrivals => {
+        this.errorSubject.next(null);
         this.airportArrivalSubject.next(arrivals);
-      }
-    );
+      }, error => {
+        this.errorSubject.next(error.message + " - " + "Maybe the proxy server is not running?");
+      });
   }
 
+  /**
+   * Makes a http request to proxy server on localhost port 3000 and gets departures out of Tallinn airport website.
+   */
   private getDeparturesOutOfHTML() {
     this.http.get('http://localhost:3000/departures', {responseType: 'text'}).pipe(
       take(1),
       map(response => {
-        let departures = [];
-
-        const htmlString = response;
-        var htmlObject = document.createElement('div');
-        htmlObject.innerHTML = htmlString;
-        const table = this.getElementByAttribute("data-flights-type", "departures", htmlObject);
-        const rows = table.querySelectorAll('.t-row:not(.hidden)');
-
-        for(var i = 1; i < rows.length; i++) {
-          const row = rows.item(i);
-          if(row.getElementsByClassName("col-1-text").item(0) != null) {
-            const time = this.getTime(row);
-            const to = this.getToFrom(row);
-            const info = this.getInfo(row);
-            const flights = this.getFlights(row);
-            const airlines = this.getAirlines(row);
-            const departure = new Departure(time, to, flights, airlines, info);
-            departures.push(departure)
-          }
-        }
-        return departures;
+        return this.getAorD(response);
       })
     ).subscribe(
       departures => {
+        this.errorSubject.next(null);
         this.airportDepartureSubject.next(departures);
-      }
-    );
+      }, error => {
+        this.errorSubject.next(error.message + " - " + "Maybe the proxy server is not running?");
+      });
   }
 
+  /**
+   * Reads Tallinn airport arrival or departure table into Arrival or Departure objects and returns an array of them.
+   * @param response - html response from proxy server.
+   */
+  private getAorD(response) {
+    let airportTable = [];
 
+    const htmlString = response;
+    var htmlObject = document.createElement('div');
+    htmlObject.innerHTML = htmlString;
+    const table = this.getElementByAttribute("data-flights-type", this.isArrival ? "arrivals" : "departures", htmlObject);
+    const rows = table.querySelectorAll('.t-row:not(.hidden)');
+
+    for(var i = 1; i < rows.length; i++) {
+      const row = rows.item(i);
+      if(row.getElementsByClassName("col-1-text").item(0) != null) {
+        const time = this.getTime(row);
+        const info = this.getInfo(row);
+        const flights = this.getFlights(row);
+        const airlines = this.getAirlines(row);
+        const toFrom = this.getToFrom(row);
+        if(this.isArrival) {
+          const arrival = new Arrival(time, toFrom, flights, airlines, info);
+          airportTable.push(arrival)
+        } else {
+          const departure = new Departure(time, toFrom, flights, airlines, info);
+          airportTable.push(departure);
+        }
+      }
+    }
+    return airportTable;
+  }
+
+  /**
+   * Gets plane arrival or departure time from row in table from Tallinn airport website.
+   * @param row - row in the table of arrivals/departures.
+   */
   private getTime(row) {
     return row.getElementsByClassName("col-1-text").item(0).textContent.trim();
   }
 
+  /**
+   * Gets airlines "to" or "from" from row in table from Tallinn airport website. Depending whether the table is arrivals or departures
+   * @param row - row in the table of arrivals/departures.
+   */
   private getToFrom(row) {
     return row.getElementsByClassName("col-2-text").item(0).textContent.trim();
   }
+
+  /**
+   * Gets information about fight from row in table from Tallinn airport website.
+   * @param row - row in the table of arrivals/departures.
+   */
   private getInfo(row) {
     return row.getElementsByClassName("col-5-text").item(0).textContent.trim();
   }
 
+  /**
+   * Gets flight numbers from row in table from Tallinn airport website.
+   * @param row - row in the table of arrivals/departures.
+   */
   private getFlights(row) {
     return this.getArray(row, "col-3-text")
   }
 
+  /**
+   * Gets airlines from row in table from Tallinn airport website.
+   * @param row - row in the table of arrivals/departures.
+   */
   private getAirlines(row) {
     return this.getArray(row, "col-4-text");
   }
 
+  /**
+   * Gets array of either flight no (col-3-text) or airline name (col-4-text) from Tallinn airport website.
+   * @param row - row in the table of arrivals/departures.
+   * @param className - eiter col-3-text or col-4-text, given above.
+   */
   private getArray(row, className) {
     var array = [];
     const rowClass = row.getElementsByClassName(className).item(0);
@@ -143,6 +177,12 @@ export class AirportService {
     return array;
   }
 
+  /**
+   * Gets elements out of html where there is a given attribute. Example [data-flights-type]="arrivals"
+   * @param attr - attribute name.
+   * @param value - attribute value.
+   * @param root - root of the html node.
+   */
   private getElementByAttribute(attr, value, root) {
     // https://stackoverflow.com/questions/6267816/getting-element-by-a-custom-attribute-using-javascript
     root = root || document.body;
